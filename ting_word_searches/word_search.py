e01335f60cf5ea51dcefee7dad5cda669770cdda
willianAD <willian.afonso013@gmail.com>
2023-06-13 18:40:42 -0300

    feat: word-search def exists_word - Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,14 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for file in instance.queue:
+        file_info = {"palavra": word,
+                     "arquivo": file["nome_do_arquivo"], "ocorrencias": []}
+        for i, line in enumerate(file["linhas_do_arquivo"], start=1):
+            if word.lower() in line.lower():
+                file_info["ocorrencias"].append({"linha": i})
+        if file_info["ocorrencias"]:
+            result.append(file_info)
+    return result
 
 
 def search_by_word(word, instance):
