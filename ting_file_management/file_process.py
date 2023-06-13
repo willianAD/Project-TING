202d92bf33d4cc7d5843d10e801570e89c11ef5b
willianAD <willian.afonso013@gmail.com>
2023-06-13 17:14:54 -0300

    feat: file-process - Requisito 3, 4 e 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,35 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in instance.queue:
+        if item["nome_do_arquivo"] == path_file:
+            return
+
+    lines = txt_importer(path_file)
+    if lines:
+        data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines
+        }
+        instance.enqueue(data)
+        sys.stdout.write(str(data))
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance.queue) > 0:
+        remove_file = instance.dequeue()
+        sys.stdout.write(
+            f"Arquivo {remove_file['nome_do_arquivo']} removido com sucesso\n")
+    else:
+        sys.stdout.write("Não há elementos\n")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position < 0 or position >= len(instance.queue):
+        sys.stderr.write("Posição inválida\n")
+    else:
+        file_info = instance.search(position)
+        sys.stdout.write(str(file_info))
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,23 +3,23 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self.__data = []
+        self.queue = []
 
     def __len__(self):
-        return len(self.__data)
+        return len(self.queue)
 
     def is_empty(self):
-        return len(self.__data) == 0
+        return len(self.queue) == 0
 
     def enqueue(self, value):
-        self.__data.append(value)
+        self.queue.append(value)
 
     def dequeue(self):
         if self.is_empty():
             raise IndexError("A fila está vazia.")
-        return self.__data.pop(0)
+        return self.queue.pop(0)
 
     def search(self, index):
-        if not 0 <= index < len(self.__data):
+        if not 0 <= index < len(self.queue):
             raise IndexError("Índice Inválido ou Inexistente")
-        return self.__data[index]
+        return self.queue[index]
